62c1cff35413d19ead687153af821710f138882b
Lizbeth <lizbth111205@gmail.com>
2026-04-12 02:31:45 -0500

    implementación de controladores MVC

---
+++ PortalInforObrasPublicas/Controllers/AccountController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PortalInforObrasPublicas.Models;
+using PortalInforObrasPublicas.Services;
+
+namespace PortalInforObrasPublicas.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UsuarioService _usuarioService;
+
+        public AccountController(UsuarioService usuarioService)
+        {
+            _usuarioService = usuarioService;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(Usuario model)
+        {
+            if (ModelState.IsValid)
+            {
+                var usuario = _usuarioService.ValidarUsuario(model.Email, model.PasswordHash);
+                if (usuario != null)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                ModelState.AddModelError("", "Correo o contraseña incorrectos.");
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(Usuario model, string ConfirmPassword)
+        {
+            if (ModelState.IsValid)
+            {
+                if (model.PasswordHash != ConfirmPassword)
+                {
+                    ModelState.AddModelError("", "Las contraseñas no coinciden.");
+                    return View(model);
+                }
+                var existe = _usuarioService.ObtenerPorEmail(model.Email);
+                if (existe != null)
+                {
+                    ModelState.AddModelError("", "El correo ya está registrado.");
+                    return View(model);
+                }
+                _usuarioService.RegistrarUsuario(model);
+                return RedirectToAction("Login");
+            }
+            return View(model);
+        }
+    }
+}
---
+++ PortalInforObrasPublicas/Controllers/ObraController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PortalInforObrasPublicas.Controllers
+{
+    public class ObraController : Controller
+    {
+        // GET: ObraController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: ObraController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ObraController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ObraController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ObraController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ObraController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ObraController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ObraController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
