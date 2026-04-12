1d1905970a80a7dcacd7190665492207d8ad0af8
Lizbeth <lizbth111205@gmail.com>
2026-04-12 06:41:46 -0500

    Se añaden controller para usuarios registrados y reportes

---
+++ PortalInforObrasPublicas/Controllers/UsuarioController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PortalInforObrasPublicas.Data;
+
+namespace PortalInforObrasPublicas.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private readonly AppDbContext _context;
+        public UsuarioController(AppDbContext context)
+        {
+            _context = context;
+        }
+        // GET: UsuarioController
+        public ActionResult Index()
+        {
+            var usuarios = _context.Usuarios.ToList();
+            return View(usuarios);
+        }
+
+        // GET: UsuarioController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: UsuarioController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UsuarioController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UsuarioController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: UsuarioController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UsuarioController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var usuario = _context.Usuarios.Find(id);
+            if (usuario == null) return NotFound();
+            return View(usuario);
+        }
+
+        // POST: UsuarioController/Delete/5
+        [HttpPost, ActionName("Delete")]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            var usuario = _context.Usuarios.Find(id);
+            if (usuario != null)
+            {
+                _context.Usuarios.Remove(usuario);
+                _context.SaveChanges();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
